86971e2c20c2f3e146be8293e739b5b8f1ea8f44
iwag <iwagame@gmail.com>
2015-03-29 18:09:01 +0900

    fetch from web

--- src/header.jsx
+++ src/header.jsx
@@ -9,7 +9,7 @@ export default React.createClass({
       <div className="ui page grid">
         <div className="column" style={{"padding-bottom": 0}}>
           <div className="title item">
-            <b>Application Name</b>
+            <b>HBFav by React.js</b>
           </div>
           <Link className="item" to="home">
             Home
@@ -19,12 +19,6 @@ export default React.createClass({
           </Link>
           <div className="right floated item">
             <i className="setting icon"/>
-          </div>
-          <div className="right floated item">
-            <div className="ui teal button">Sign Up</div>
-          </div>
-          <div className="right floated item">
-            <div className="ui button">Log in</div>
           </div>
         </div>
       </div>
