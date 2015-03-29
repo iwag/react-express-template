79b5c1354900612ecdfbf0bf3190fb3caa75c5dc
iwag <iwagame@gmail.com>
2015-03-29 15:22:11 +0900

    cjsx => jsx

--- src/client.jsx
+++ src/client.jsx
@@ -1,13 +1,19 @@
 window.$ = window.jQuery = require('jquery')
 require('semantic-ui-css/semantic')
-React = require('react/addons')
-Router = require('react-router')
-Header = require('./header')
+var React = require('react/addons')
+var Router = require('react-router')
+var Header = require('./header')
 
-{Routes, Route, DefaultRoute, Link} = Router
 
-Home = React.createClass
-  render: ->
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var Routes = Router.Routes;
+var RouteHandler = Router.RouteHandler;
+
+var Home = React.createClass({
+                              render() {
+                              return (
     <div className="column">
       <div className="ui segment">
         <h1 className="ui header">
@@ -18,30 +24,47 @@ Home = React.createClass
         </h1>
       </div>
     </div>
+    );
+    }
+    });
 
-About = React.createClass
-  render: ->
+var About = React.createClass({
+  render() {
+  return (
     <div className="column">
       <div className="ui segment">
         <h4 className="ui black header">This is the about page.</h4>
       </div>
     </div>
+    );
+    }
+    });
 
-Main = React.createClass
-  render: ->
+
+var Main = React.createClass({
+                              render() {
+                              return (
     <div>
       <Header/>
       <div className="ui page grid">
-        <@props.activeRouteHandler/>
+        <this.props.activeRouteHandler/>
       </div>
     </div>
+    );
+    }
+    });
 
-routes =
+
+var routes = (
   <Routes location="hash">
     <Route path="/" handler={Main}>
       <DefaultRoute name="home" handler={Home}/>
       <Route name="about" handler={About}/>
     </Route>
   </Routes>
+)
 
-$ -> React.renderComponent(routes, document.body)
+$(function() {
+	  return React.renderComponent(routes, document.body);
+});
+
